8278748089b0e7baa40003b5e4c68e9bdb9136b0
Ayman-M-Ali <ayman.mohmd.ali@gmail.com>
2023-04-25 22:21:46 +0300

    Build Doob Agency

---
+++ main.js
@@ -0,0 +1,40 @@
+function show() {
+  let projectsHidden = document.getElementById("projects-hidden");
+  let more = document.querySelector(".show-more");
+  let less = document.querySelector(".show-less");
+  if (projectsHidden.style.display === "none") {
+    projectsHidden.style = "display: grid";
+    more.style.display = "none";
+    less.style.display = "block";
+  } else {
+    projectsHidden.style.display = "none";
+  }
+}
+function less() {
+  let less = document.querySelector(".show-less");
+  let more = document.querySelector(".show-more");
+  let projectsHidden = document.getElementById("projects-hidden");
+  if (projectsHidden.style.display === "grid") {
+    projectsHidden.style.display = "none";
+    less.style.display = "none";
+    more.style.display = "block";
+  } else {
+    projectsHidden.style.display = "none";
+  }
+}
+
+const blogHidden = document.querySelector('.stories-hidden');
+const blogShows = document.querySelector('.stories-shows');
+const arrRight = document.querySelector('.arr-right');
+const arrLeft = document.querySelector('.arr-left');
+
+arrRight.addEventListener('click', () => {
+  blogHidden.classList.toggle('active');
+  
+  blogHidden.style.transform = "translateX(0)";
+  blogShows.style.transform = "translateX(-250%)";
+});
+arrLeft.addEventListener('click', () => {
+  blogHidden.style.transform = "translateX(250%)";
+  blogShows.style.transform = "translateX(0)";
+});
